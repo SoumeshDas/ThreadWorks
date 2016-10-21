93f9a807b45dcab65371ce02adb08600cf994d8d
SoumeshDas <cse.soumeshdas@gmail.com>
2016-10-21 14:13:03 +0530

    First Commit

---
+++ AsyncAndAwaitExample/AsyncAndAwaitExample/Implementation/GetBucketDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwaitExample.Implementation
+{
+    class GetBucketDetails
+    {
+        public static Tuple<string,int> VideoCallerIntergotaion()
+        {
+            Thread.Sleep(30);
+            Tuple<string, int> tuple = new Tuple<string, int>("Hellow", 1);
+            return tuple;
+        }
+    }
+}
