4a9255a7c0c358b574df80bc14f0c79689ea9414
SoumeshDas <cse.soumeshdas@gmail.com>
2016-10-25 01:35:47 +0530

    Implemented Unity

---
+++ AsyncAndAwaitExample/AsyncAndAwaitExample/Implementation/INStockPrice.cs
@@ -0,0 +1,20 @@
+﻿using AsyncAndAwaitExample.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwaitExample.Implementation
+{
+    class INStockPrice : IPrice
+    {
+        public decimal NegotiatePrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MyProperty { get; set; }
+
+    }
+}
--- AsyncAndAwaitExample/AsyncAndAwaitExample/Program.cs
+++ AsyncAndAwaitExample/AsyncAndAwaitExample/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using AsyncAndAwaitExample.Abstraction;
+using AsyncAndAwaitExample.Implementation;
+using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace AsyncAndAwaitExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var unity = new UnityContainer();
+            unity.RegisterType<IPrice, INStockPrice>();
+            var instockprice = unity.Resolve<IPrice>();
+            Console.WriteLine("Application Details using  UNITY " + instockprice.ToString());
+            Console.ReadLine();
+            
             
         }
 
